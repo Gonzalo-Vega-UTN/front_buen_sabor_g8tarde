523e54e5b02191cbb65201ff754d10849f5051f0
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-06-10 23:07:42 -0300

    carrousel logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,38 +18,35 @@ const Header: React.FC = () => {
   };
 
   return (
-    <Container>
-      <Navbar expand="lg">
-        <Navbar.Brand onClick={() => handleNavigation('/')}></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" className="position-relative">
-          <Nav className="me-auto my-2 my-lg-0 position-absolute top-50 end-0 translate-middle-y px-5">
-            <NavDropdown title={<Icon.PersonCircle size={32} />} id="navbarScrollingDropdown">
-              {isAuthenticated && (
-                <>
-                  <NavDropdown.Item onClick={() => handleNavigation('/perfil')}>
-                    Editar Perfil
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                </>
-              )}
-              {userRol === 'Admin' && (
-                <>
-                  <NavDropdown.Item onClick={() => handleNavigation('/productos')}>
-                    Productos
-                  </NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => handleNavigation('/ingredientes')}>
-                    Ingredientes
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                </>
-              )}
-              {isAuthenticated ? <BotonLogout /> : <BotonLogin />}
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container>
+    <Navbar expand="lg">
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll" className="position-relative">
+        <Nav className="me-auto my-2 my-lg-0 position-absolute top-50 end-0 translate-middle-y px-5">
+          <NavDropdown title={<Icon.PersonCircle size={32} />} id="navbarScrollingDropdown">
+            {isAuthenticated && (
+              <>
+                <NavDropdown.Item onClick={() => handleNavigation('/perfil')}>
+                  Editar Perfil
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+              </>
+            )}
+            {userRol === 'Admin' && (
+              <>
+                <NavDropdown.Item onClick={() => handleNavigation('/productos')}>
+                  Productos
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleNavigation('/ingredientes')}>
+                  Ingredientes
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+              </>
+            )}
+            {isAuthenticated ? <BotonLogout /> : <BotonLogin />}
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,75 +1,109 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import { CategoriaService } from '../../services/CategoriaService';
 import { Categoria } from '../../entities/DTO/Categoria/Categoria';
 import Lista from '../../MioImport/lista';
+import CarouselComponent from '../Carousel/Carousel';
+import { useAuth } from '../../Auth/Auth';
+import logo from '../../assets/images/Buen sabor logo 1.png'; // Importa el logo
 
-const Home = () => {
-    const [categorias, setCategorias] = useState<Categoria[]>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [selectedCategoryId, setSelectedCategoryId] = useState<number | undefined>(undefined);
+const Home: React.FC = () => {
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [, setError] = useState<string | null>(null);
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number | undefined>(undefined);
+  const { isAuthenticated } = useAuth();
+  const [featuredProducts, setFeaturedProducts] = useState([
+    {
+      name: 'Producto 1',
+      description: 'Descripción del producto 1',
+      imageUrl: '/path/to/image1.jpg'
+    },
+    {
+      name: 'Producto 2',
+      description: 'Descripción del producto 2',
+      imageUrl: '/path/to/image2.jpg'
+    },
+    {
+      name: 'Producto 3',
+      description: 'Descripción del producto 3',
+      imageUrl: '/path/to/image3.jpg'
+    }
+  ]);
 
-    // Función para obtener categorías
-    const fetchCategories = async () => {
-        try {
-            const data = await CategoriaService.obtenerCategorias();
-            setCategorias([{
-                id: undefined, denominacion: 'Todos', imagen: '',
-                alta: false
-            }, ...data]); // Agrega la categoría "Todos"
-        } catch (error) {
-            if (error instanceof Error) {
-                setError(error.message);
-            } else {
-                setError("An unknown error occurred");
-            }
-        }
-    };
+  const fetchCategories = async () => {
+    try {
+      const data = await CategoriaService.obtenerCategorias();
+      setCategorias([{
+        id: undefined, denominacion: 'Todos', imagen: '',
+        alta: false,
+        subCategorias: []
+      }, ...data]);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unknown error occurred");
+      }
+    }
+  };
 
-    useEffect(() => {
-        fetchCategories();
-    }, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    const defaultImageUrl = 'https://http2.mlstatic.com/storage/sc-seller-journey-backoffice/images-assets/234940675890-Sucursales--una-herramienta-para-mejorar-la-gesti-n-de-tus-puntos-de-venta.png';
+  const defaultImageUrl = 'https://http2.mlstatic.com/storage/sc-seller-journey-backoffice/images-assets/234940675890-Sucursales--una-herramienta-para-mejorar-la-gesti-n-de-tus-puntos-de-venta.png';
 
-    const handleCategoryClick = (categoryId: number | undefined) => {
-        setSelectedCategoryId(categoryId);
-    };
+  const handleCategoryClick = (categoryId: number | undefined) => {
+    setSelectedCategoryId(categoryId);
+  };
 
-    return (
-        <div className="container mt-5">
-            {/* Search Bar and Header */}
-            <div className="row mb-4">
-                <div className="col-md-8">
-                    <input type="text" className="form-control" placeholder="Buscar comida..." />
-                </div>
-                <div className="col-md-4">
-                    <Header />
-                </div>
-            </div>
-            {/* Categories */}
-            <div className="row mb-4">
-                {categorias.map(category => (
-                    <div
-                        className={`col-3 text-center ${selectedCategoryId === category.id ? 'selected' : ''}`}
-                        key={category.id === undefined ? 'all' : category.id}
-                        onClick={() => handleCategoryClick(category.id)}
-                    >
-                        <img
-                            src={category.imagen || defaultImageUrl}
-                            className="rounded-circle"
-                            alt={category.denominacion}
-                            width="100"
-                        />
-                        <h5 className="mt-2">{category.denominacion}</h5>
-                    </div>
-                ))}
-            </div>
-
-            {/* Featured Products */}
-            <Lista selectedCategoryId={selectedCategoryId} /> {/* Pasa la categoría seleccionada al componente Lista */}
+  return (
+    <div className="container mt-5">
+      {/* Search Bar and Header */}
+      <div className="row mb-4 align-items-center">
+        <div className="col-md-1">
+          {!isAuthenticated && (
+            <img src={logo} alt="Logo" className="logo" style={{ maxHeight: '120px' }} />
+          )}
         </div>
-    );
+        <div className="col-md-7">
+          <input type="text" className="form-control" placeholder="Buscar comida..." />
+        </div>
+        <div className="col-md-4">
+          <Header />
+        </div>
+      </div>
+      
+      {/* Carousel and Introduction Box */}
+      <div className="row mb-4">
+        <div className="col-12">
+          <CarouselComponent products={featuredProducts} />
+        </div>
+      </div>
+      
+      {/* Categories */}
+      <div className="row mb-4">
+        {categorias.map(category => (
+          <div
+            className={`col-3 text-center ${selectedCategoryId === category.id ? 'selected' : ''}`}
+            key={category.id === undefined ? 'all' : category.id}
+            onClick={() => handleCategoryClick(category.id)}
+          >
+            <img
+              src={category.imagen || defaultImageUrl}
+              className="rounded-circle"
+              alt={category.denominacion}
+              width="100"
+            />
+            <h5 className="mt-2">{category.denominacion}</h5>
+          </div>
+        ))}
+      </div>
+
+      {/* Featured Products */}
+      <Lista selectedCategoryId={selectedCategoryId} />
+    </div>
+  );
 };
 
 export default Home;
