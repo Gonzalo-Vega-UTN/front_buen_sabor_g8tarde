edbce375063f15d21dff61e66341757f2473d767
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-06-25 11:42:28 -0300

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,14 +10,12 @@ import { CategoriaService } from '../../services/CategoriaService';
 import { ProductServices } from '../../services/ProductServices';
 import { ArticuloManufacturado } from '../../entities/DTO/Articulo/ManuFacturado/ArticuloManufacturado';
 import { useAuth } from '../../Auth/Auth';
-import logo from '../../assets/images/Buen sabor logo 1.png';
 import { useCart } from '../Carrito/ContextCarrito';
 import Carrito from '../Carrito/carrito';
-import './RoundCard.css';
 import { useNavigate } from 'react-router-dom';
 import ArticuloInsumoService from '../../services/ArticuloInsumoService';
 import { Articulo } from '../../entities/DTO/Articulo/Articulo';
-
+import { Cart, CartFill } from 'react-bootstrap-icons';  // Importación de íconos
 
 const Home: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -31,9 +29,10 @@ const Home: React.FC = () => {
   const [productos, setProductos] = useState<Articulo[]>([]);
   const [selectedCategoryId, setSelectedCategoryId] = useState<number | undefined>(undefined);
   const { isAuthenticated } = useAuth();
-  const { agregarAlCarrito } = useCart(); // Usa el hook useCart para agregar al carrito
-  const [subCategoriaSelected, setSubCategoriaSelected] = useState<boolean>(false)
+  const { agregarAlCarrito } = useCart();
+  const [subCategoriaSelected, setSubCategoriaSelected] = useState<boolean>(false);
   const navigate = useNavigate();
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   useEffect(() => {
     fetchEmpresas();
@@ -66,7 +65,7 @@ const Home: React.FC = () => {
   const fetchCategoriasPadresBySucursal = async (idSucursal: number) => {
     try {
       setLoading(true);
-      setSubCategoriaSelected(false)
+      setSubCategoriaSelected(false);
       const data = await CategoriaService.obtenerCategoriasPadre(idSucursal.toString());
       setCategorias(data);
     } catch (error) {
@@ -75,13 +74,14 @@ const Home: React.FC = () => {
       setLoading(false);
     }
   };
+
   const fetchCategoriaById = async (idCategoria: number) => {
     try {
       setLoading(true);
       const data = await CategoriaService.obtenerCategoriaById(idCategoria);
       if (data.subCategorias && data.subCategorias.length > 0) {
         setCategorias(data.subCategorias);
-        setSubCategoriaSelected(true) //Controla el boton para volver
+        setSubCategoriaSelected(true);
       }
     } catch (error) {
       console.error('Error fetching categorias padre:', error);
@@ -98,10 +98,9 @@ const Home: React.FC = () => {
         setProductos([...manufacturados, ...insumos]);
       } catch (error) {
         console.error('Error fetching productos:', error);
-      } finally {
       }
     } else {
-      setCurrentStep(2)
+      setCurrentStep(2);
     }
   };
 
@@ -115,149 +114,121 @@ const Home: React.FC = () => {
     setSelectedSucursal(sucursal);
     fetchCategoriasPadresBySucursal(sucursal.id);
     setCurrentStep(3);
-
   };
 
   const selectCategoria = (categoria: Categoria) => {
     setSelectedCategoryId(categoria.id);
     fetchProductos(categoria.id);
-    fetchCategoriaById(categoria.id)
+    fetchCategoriaById(categoria.id);
   };
 
-
-  // if (loading) {
-  //   return <div className="container">Cargando...</div>;
-  // }
+  const handleAgregarAlCarrito = (producto: Articulo) => {
+    agregarAlCarrito(producto);
+    setIsCartOpen(true);
+  };
 
   return (
-    <div className="container-fluid mt-5 home-background text-light">
-      <div className="row mb-4 align-items-center">
-        <div className="col-md-1">
-          {!isAuthenticated && (
-            <img src={logo} alt="Logo" className="logo" />
-          )}
-        </div>
-        <div className="col-md-7">
-          <input type="text" className="form-control search-bar" placeholder="Buscar comida..." />
-        </div>
-      </div>
+    <div className="home-container">
+      <div className="main-content">
+        <input type="text" className="form-control search-bar mb-4" placeholder="Buscar comida..." />
+        
+        {currentStep === 1 && (
+          <Container>
+            <h1 className="section-title">Seleccionar Empresa</h1>
+            <Row>
+              {empresas.map((empresa) => (
+                <Col key={empresa.id} sm={12} md={6} lg={4} className="mb-4">
+                  <Card onClick={() => selectEmpresa(empresa)} style={{ cursor: 'pointer' }}>
+                    <Card.Img variant="top" src={empresa.imagenes[0] ? empresa.imagenes[0].url : "https://via.placeholder.com/150"} />
+                    <Card.Body>
+                      <Card.Title>{empresa.nombre}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        )}
 
-      {currentStep === 1 && (
-        <Container>
-          <h1>Seleccionar Empresa</h1>
-          <Row>
-            {empresas.map((empresa) => (
-              <Col key={empresa.id} sm={12} md={6} lg={4} className="mb-4">
-                <Card
-                  onClick={() => selectEmpresa(empresa)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  <Card.Img
-                    variant="top"
-                    src={empresa.imagenes[0] ? empresa.imagenes[0].url : "https://via.placeholder.com/150"}
-                  />
-                  <Card.Body>
-                    <Card.Title>{empresa.nombre}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      )}
+        {currentStep === 2 && (
+          <Container>
+            <Button onClick={() => setCurrentStep(1)}>Cambiar Empresa</Button>
+            <h2 className="section-title">Seleccionar Sucursal de {selectedEmpresa?.nombre}</h2>
+            <Row>
+              {sucursales.map((sucursal) => (
+                <Col key={sucursal.id} sm={12} md={6} lg={4} className="mb-4">
+                  <Card onClick={() => selectSucursal(sucursal)} style={{ cursor: 'pointer' }}>
+                    <Card.Img variant="top" src={sucursal.imagenes[0] ? sucursal.imagenes[0].url : "https://via.placeholder.com/150"} />
+                    <Card.Body>
+                      <Card.Title>{sucursal.nombre}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        )}
 
-      {currentStep === 2 && (
-        <Container>
-          <Button onClick={() => setCurrentStep(1)}>Cambiar Empresa</Button>
-          <h2>Seleccionar Sucursal de {selectedEmpresa?.nombre}</h2>
-          <Row>
-            {sucursales.map((sucursal) => (
-              <Col key={sucursal.id} sm={12} md={6} lg={4} className="mb-4">
-                <Card
-                  onClick={() => selectSucursal(sucursal)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  <Card.Img variant="top" src={sucursal.imagenes[0] ? sucursal.imagenes[0].url : "https://via.placeholder.com/150"} />
-                  <Card.Body>
-                    <Card.Title>{sucursal.nombre}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      )}
-
-      {currentStep === 3 && (
-        <Container>
-          <Button onClick={() => {
-            setCurrentStep(2)
-            setProductos([])
-          }}>Cambiar Sucursal</Button>
-          <h1>Seleccionar Categoría</h1>
-          <Row>
-            {subCategoriaSelected && <Button variant='secondary' style={{ width: "100px", height: '100px', borderRadius: "50%" }} onClick={() => fetchCategoriasPadresBySucursal(selectedSucursal?.id)}>Volver</Button>}
-            {categorias.map((categoria) => (
-              <Col key={categoria.id} className="mb-4">
-                <Card className="rounded-card" onClick={() => selectCategoria(categoria)} style={{ cursor: 'pointer' }}>
-                  <Card.Img
-                    variant="top"
-                    src={categoria.imagenes[0] ? categoria.imagenes[0].url : "https://via.placeholder.com/150"}
-                    className="rounded-img"
-                  />
-                  <Card.Body>
-                    <Card.Title>{categoria.denominacion}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-          <h2>Productos</h2>
-          <Row>
-            {
-              productos.length !== 0 ? (
+        {currentStep === 3 && (
+          <Container>
+            <Button onClick={() => { setCurrentStep(2); setProductos([]); }}>Cambiar Sucursal</Button>
+            <h1 className="section-title">Seleccionar Categoría</h1>
+            <Row className="mb-4">
+              {subCategoriaSelected && (
+                <Col>
+                  <Button variant='secondary' style={{ width: "80px", height: '80px', borderRadius: "50%" }} onClick={() => fetchCategoriasPadresBySucursal(selectedSucursal?.id)}>
+                    Volver
+                  </Button>
+                </Col>
+              )}
+              {categorias.map((categoria) => (
+                <Col key={categoria.id}>
+                  <div className={`category ${selectedCategoryId === categoria.id ? 'selected' : ''}`} onClick={() => selectCategoria(categoria)}>
+                    <img src={categoria.imagenes[0] ? categoria.imagenes[0].url : "https://via.placeholder.com/80"} alt={categoria.denominacion} className="category-image" />
+                    <p>{categoria.denominacion}</p>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+            <h2 className="section-title">Productos</h2>
+            <div className="products-container">
+              {productos.length > 0 ? (
                 productos.map((producto) => (
-                  <Col key={producto.id} sm={12} md={6} lg={4} className="mb-4">
-                    <Card className="product-card">
-                      <Card.Img
-                        variant="top"
-                        src={producto.imagenes.length > 0 ? producto.imagenes[0].url : "https://via.placeholder.com/150"}
-                        className="product-image"
-                      />
-                      <Card.Body>
-                        <Card.Title>{producto.denominacion}</Card.Title>
-                        <Card.Text>{producto instanceof ArticuloManufacturado ? producto.descripcion : ""}</Card.Text>
-                        <Card.Text>Precio: ${producto.precioVenta}</Card.Text>
-                        {isAuthenticated ?
-                          <Button variant="primary" className="add-to-cart-button" onClick={() => agregarAlCarrito(producto)}>
-                            Añadir al carrito
-                          </Button>
-                          :
-                          <Button variant="primary" className="add-to-cart-button" onClick={() => navigate("/registro")}>
-                            Login
-                          </Button>
-                        }
-                      </Card.Body>
-                    </Card>
-                  </Col>
+                  <div key={producto.id} className="product-card">
+                    <img src={producto.imagenes[0] ? producto.imagenes[0].url : "https://via.placeholder.com/100"} alt={producto.denominacion} className="product-image" />
+                    <h3>{producto.denominacion}</h3>
+                    <p>{producto instanceof ArticuloManufacturado ? producto.descripcion : ""}</p>
+                    <p>Precio: ${producto.precioVenta}</p>
+                    {isAuthenticated ? (
+                      <Button variant="primary" onClick={() => handleAgregarAlCarrito(producto)}>
+                        Añadir al carrito
+                      </Button>
+                    ) : (
+                      <Button variant="primary" onClick={() => navigate("/registro")}>
+                        Login
+                      </Button>
+                    )}
+                  </div>
                 ))
               ) : (
-                selectedCategoryId ? (
-                  <p>
-                    No hay productos de Categoria{" "}
-                    {categorias.filter((cat) => cat.id === selectedCategoryId)[0]?.denominacion}
-                  </p>
-                ) : null
-              )
-            }
+                selectedCategoryId && <p>No hay productos en esta categoría.</p>
+              )}
+            </div>
+          </Container>
+        )}
+      </div>
 
-          </Row>
-        </Container>
-      )}
+      <Button onClick={() => setIsCartOpen(!isCartOpen)} className="toggle-cart-btn">
+        {isCartOpen ? <CartFill size={24} /> : <Cart size={24} />}
+      </Button>
 
-      {selectedSucursal && isAuthenticated && ( // Condiciona el renderizado del carrito
-        <Carrito actualizarLista={() => fetchProductos(selectedCategoryId!)} />
-      )}
+      <div className={`cart-sidebar ${isCartOpen ? 'open' : ''}`}>
+        <Carrito 
+          actualizarLista={() => fetchProductos(selectedCategoryId!)}
+          isOpen={isCartOpen}
+          setIsOpen={setIsCartOpen}
+        />
+      </div>
     </div>
   );
 };
