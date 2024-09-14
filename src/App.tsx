58c98fceaf59a34597a7557eb6c5395d38802582
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-09-14 19:31:26 -0300

    Update barra superior

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-
+import { useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
-
 import Footer from "./components/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import SucursalDropdown from "./components/SucursalDropdown/SucursalDropdown";
 
-const AppContent = () => (
-  <div className="container-fluid p-0 layout">
-    <div className="row g-0">
-      <div className="col-md-2">
-        <Sidebar />
-        <SucursalDropdown />
-      </div>
-      <div className="col-md-10 d-flex flex-column min-vh-100">
-        <main className="flex-grow-1">
-          <AppRoutes />
-        </main>
-        <Footer />
+const AppContent = () => {
+  const location = useLocation();
+  
+  return (
+    <div className="container-fluid p-0 layout">
+      <div className="row g-0">
+        <div className="col-md-2">
+          <Sidebar />
+          {/* Si la ruta no es /empresas, muestra el SucursalDropdown */}
+          {location.pathname !== "/empresas" && <SucursalDropdown />}
+        </div>
+        <div className="col-md-10 d-flex flex-column min-vh-100">
+          <main className="flex-grow-1">
+            <AppRoutes />
+          </main>
+          <Footer />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const App = () => <AppContent />;
 
