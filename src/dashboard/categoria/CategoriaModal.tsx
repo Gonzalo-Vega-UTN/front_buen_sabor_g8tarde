f2b8f4aab6c0e64b06748107af4e1978b802e1e7
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-11-03 19:51:50 -0300

    fix warnings

--- src/dashboard/categoria/CategoriaModal.tsx
+++ src/dashboard/categoria/CategoriaModal.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
-import Alert from "react-bootstrap/Alert";
 import { Categoria } from "../../entities/DTO/Categoria/Categoria";
 import { CategoriaService } from "../../services/CategoriaService";
 import ImagenCarousel from "../../components/generic/carousel/ImagenCarousel";
